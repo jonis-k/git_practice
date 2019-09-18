b676bca5fec8993d2f6d6db8e7bf8fc43468a53c
jonis-k <49103936+jonis-k@users.noreply.github.com>
2019-09-18 11:35:53 +0300

    Add files via upload

---
+++ testscript1.py
@@ -0,0 +1,38 @@
+from ats import aetest
+
+
+class ScriptCommonSetup(aetest.CommonSetup):
+
+	@aetest.subsection
+	def check_script_arguments(self):
+		pass
+
+	@aetest.subsection
+	def connect_to_devices(self):
+		pass
+
+	@aetest.subsection
+	def configure_interfaces(self):
+		pass
+
+
+class FirstTestcase(aetest.Testcase):
+
+	@aetest.test
+	def print_hello(self):
+		print("Hello")
+
+
+class ScriptCommonCleanup(aetest.CommonCleanup):
+
+	@aetest.subsection
+	def remove_testbed_configurations(self):
+		pass
+
+	@aetest.subsection
+	def disconnect_from_devices(self):
+		pass
+
+
+if __name__ == '__main__':
+	aetest.main()
